ea8f5401b0a3b6ba5e7c22606c493771135c2a77
Juan <juandres.10@gmail.com>
2019-05-29 21:58:30 -0400

    actualizacion

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,35 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
-import { Usuario } from '../../../back-end/models/usuario.js';
-
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css'],
-  providers: [AuthService]
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
 
-  usuario:Usuario;
-
-  constructor(protected authService: AuthService) { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
-
-
 
   onSubmit(form: NgForm){
     const email=form.value.email;
     const password1=form.value.password;
     const password2=form.value.password2;
+    const phone=form.value.phone;
+    const username=form.value.username;
     console.log("hello");
     if(password1 === password2){
       console.log("hello");
-      this.authService.signUpUser(email, password1);
-
+      this.authService.signUpUser(email, password1, username, phone);
       
     }
   }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,38 +1,24 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireModule } from '@angular/fire';
-import { User } from  'firebase';
-import { UsuarioService } from './../usuario/usuario.service';
-import { Usuario } from '../../../../back-end/models/usuario.js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  token: boolean;
-  user: User;
-  usuario: Usuario;
+  token: string;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth, private FB: AngularFireModule, private usuarioService: UsuarioService) { 
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        this.user = user;
-        localStorage.setItem('user', JSON.stringify(this.user));
-      } else {
-        localStorage.setItem('user', null);
-      }
-    })
-  }
+  constructor(private router: Router) { }
 
-  signUpUser(email: string, password: string){
+  signUpUser(email: string, password: string, username: string, phone: string){
     firebase.auth().createUserWithEmailAndPassword(email, password).then(
       response => {
         this.router.navigate(['inicio']);
-        this.token=true;
-        this.usuarioService.postUsuarios(this.usuario);
+        firebase.auth().currentUser.getIdToken().then(
+          (token: string) => this.token=token
+          
+        )
       }
     )
     .catch(
@@ -40,25 +26,34 @@ export class AuthService {
     );
   }
 
-  async signInUser(email: string, password: string) {
-    try {
-      await  this.afAuth.auth.signInWithEmailAndPassword(email, password)
-      this.router.navigate(['./inicio']);
-  } catch (e) {
-      alert("Error!"  +  e.message);
-  }
+  signInUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => {
+          this.router.navigate(['inicio']);
+          firebase.auth().currentUser.getIdToken().then(
+            (token: string) => this.token = token
+
+          )
+        }
+      ).catch(
+        error => console.log(error)
+      );
   }
 
+  getToken() {
+    firebase.auth().currentUser.getIdToken().then(
+      (token: string) => this.token = token
+    );
+    return this.token;
+  }
 
   isAuthenticated(){
-    const  user  =  JSON.parse(localStorage.getItem('user'));
-    return  user  !==  null;
+    return this.token!=null
   }
 
-  async logoutUser() {
-    await this.afAuth.auth.signOut();
-    localStorage.removeItem('user');
-    this.router.navigate(['./login']);
+  logoutUser() {
+    firebase.auth().signOut();
+    this.token = null;
   }
-
 }
