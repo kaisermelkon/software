75d015ccc8d93b46c77d5953104cacc2f9a71b8e
Daniel Velasquez Iturrate <daniel.velasquez@correo.unimet.edu.ve>
2019-06-09 18:17:44 -0400

    Perfil, Modales de Editar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
-import {NgForm} from '@angular/forms';
-import {GrupoService} from '../../shared/services/grupo/grupo.service';
-import {DireccionService} from './../../shared/services/direccion/direccion.service';
-import {Grupo} from '../../shared/models/Grupos';
-import {Direccion} from './../../shared/models/Direccion';
+import { NgForm } from '@angular/forms';
+import { GrupoService } from '../../shared/services/grupo/grupo.service';
+import { DireccionService } from './../../shared/services/direccion/direccion.service';
+import { Grupo } from '../../shared/models/Grupos';
+import { Direccion } from './../../shared/models/Direccion';
 import { UsuarioService } from 'src/app/shared/services/usuario/usuario.service';
 
 @Component({
@@ -18,11 +18,11 @@ export class NavbarComponent implements OnInit {
   grupo: Grupo;
   direccion: Direccion;
   direccionId: any;
-  
 
-  constructor(protected authService: AuthService, private grupoService: GrupoService, private direccionService: DireccionService, private usuarioService: UsuarioService) { 
-    console.log(this.authService.token+"hello");
-    this.grupo= new Grupo();
+
+  constructor(protected authService: AuthService, private grupoService: GrupoService, private direccionService: DireccionService, private usuarioService: UsuarioService) {
+    console.log(this.authService.token + "hello");
+    this.grupo = new Grupo();
     this.direccion = new Direccion();
   }
 
@@ -31,24 +31,28 @@ export class NavbarComponent implements OnInit {
 
   async onSubmit(form: NgForm) {
     this.grupo.nombre = form.value.nombre;
-    this.grupo.codigo=Math.floor(10000000 + Math.random() * 90000000);
-    this.direccion.municipio=form.value.municipio;
-    this.direccion.descripcion=form.value.descripcion;
-    this.direccion.urbanizacion=form.value.urbanizacion;
+    this.grupo.codigo = Math.floor(10000000 + Math.random() * 90000000);
+    this.direccion.municipio = form.value.municipio;
+    this.direccion.descripcion = form.value.descripcion;
+    this.direccion.urbanizacion = form.value.urbanizacion;
     await this.direccionService.createDireccion(this.direccion);
-    //await this.sleep(1000);
+    await this.sleep(1000);
     await this.direccionService.getDireccionId(this.direccion.municipio, this.direccion.urbanizacion, this.direccion.descripcion).subscribe(res => {
-      this.direccionId=res;
+      this.direccionId = res;
     }, err => console.log(err));
     await this.sleep(1000);
     //while(this.grupoService.getGruposCodigo(this.grupo.codigo.toString())){
-      
-      //this.grupo.codigo=Math.floor(10000000 + Math.random() * 90000000);
+
+    //this.grupo.codigo=Math.floor(10000000 + Math.random() * 90000000);
     //}
-    this.grupo.administradorId=this.usuarioService.usuario.id;
-    this.grupo.direccionId=this.direccionId
+    this.grupo.administradorId = this.usuarioService.usuario.id;
+    this.grupo.direccionId = this.direccionId;
     await this.grupoService.createGrupo(this.grupo);
     await this.sleep(1000);
+  }
+
+  async onSubmit2(form: NgForm){
+    
   }
 
   Open() {
