0d02a622775cd42657eb035c6dff7df55d9acc98
Daniel Velasquez Iturrate <daniel.velasquez@correo.unimet.edu.ve>
2019-06-13 15:18:08 -0400

    Cualquier Vaina

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/shared/models/Usuario';
+import { UsuarioService } from 'src/app/shared/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    console.log(this.usuarioService.usuario);
   }
 
 }
