83a38e953f16fec42f6b708b4c8271f613d72a92
kaisermelkon <kaisermelkon@gmail.com>
2019-07-09 19:07:02 -0400

    chenage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,13 @@
    * @ignore
  */
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "...",
+    authDomain: "project.firebaseapp.com",
+    databaseURL: "https://project.firebaseio.com",
+    projectId: "project",
+    storageBucket: "project.appspot.com",
+    messagingSenderId: "..."
+  }
 };
